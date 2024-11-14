6e96c0d7d2052638de045cd0330ce0af7b251fac
pervin0527 <zfbtldnz@gmail.com>
2024-11-14 21:50:02 +0900

    Updated 2024.11.14

---
+++ Project/Cpp3_1/test.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main()
+{
+    printf("Hello %d\n", 10);
+    printf("%d + %d = %i\n", 10, 7, 10+7);
+
+    // freopen("output.txt", "w", stdout);
+    // printf("HaHaHa");
+
+    int num = 0;
+    scanf("%d", &num);
+    printf("%d", num);
+}
